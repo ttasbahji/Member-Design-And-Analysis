81c85866ec1f6b842b7eb6b3aff7d87a55f73c60
mohamad ali kandkji <m.ali.kandkji@gmail.com>
2019-01-01 15:37:03 +0300

    Done from the First Stage OF the Project

---
+++ StructuralMemberDesignandAnalysis/Classes/Louver/Louver.cs
@@ -0,0 +1,75 @@
+﻿using System;
+namespace StructuralMemberDesignandAnalysis.Classes.Louver
+
+{
+    public class Louver : Member
+    {
+        public Louver() { }
+
+
+
+        public override void AnalyticCompressionCapacity()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void AnalyticMomentCapacity()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void AnalyticShearCapacity()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void AnalyticTensionCapacity()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void DesignCompressionCapacity()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void DesignMomentCapacity()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void DesignShearCapacity()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void DesignTensionCapacity()
+        {
+            throw new NotImplementedException();
+        }
+
+
+       
+
+        public class LouverAnchors : Member.Connection
+        {
+
+
+        }
+
+        public class BuildUpLouver : Member.BuildUpSetion
+        {
+
+
+        }
+
+        public class ReadSectionLouver : Member.ReadySection
+        {
+
+
+        }
+
+
+    }
+
+}
