81cbea502adcb7a3639fea989b8c5f06363fdf85
mohamad ali kandkji <m.ali.kandkji@gmail.com>
2019-01-06 20:54:17 +0300

    Corrected Some Stuff and Tested the code.

---
+++ StructuralMemberDesignandAnalysis/Classes/MemberDesignText/LouverStrings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace StructuralMemberDesignandAnalysis.Classes.MemberDesignText
+{
+    public class LouverStrings
+    {
+
+        public const string TwoWayLouverText = "TwoWayLouver";
+        public const string OneWayLouverText = "OneWayLouver";
+
+
+        public const string NorthSideConnection = "North side Anchor";
+        public const string SouthSideConnection = "South side Anchor";
+        public const string EastSideConnection = "East side Anchor";
+        public const string WestSideConnection = "West side Anchor";
+
+    }
+}
